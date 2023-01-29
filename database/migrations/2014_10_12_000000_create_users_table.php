bc33c01818c0b3f21c63909a10c76c7e5efff909
Gabriel Pérez <gapc87@gmail.com>
2023-01-29 19:55:34 +0100

    master

---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,52 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('teams', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->text('description');
+            $table->integer('hero_1')->nullable();
+            $table->integer('hero_2')->nullable();
+            $table->integer('hero_3')->nullable();
+            $table->integer('hero_4')->nullable();
+            $table->integer('hero_5')->nullable();
+            $table->integer('hero_6')->nullable();
+            $table->timestamps();
+        });
+
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->rememberToken();
+            $table->timestamps();
+
+            $table->foreignId('team_id')->constrained();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('teams');
+        Schema::dropIfExists('users');
+    }
+}
