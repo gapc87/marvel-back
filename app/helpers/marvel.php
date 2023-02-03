287722dff4d1f6ece7250a2543051229063350a0
Gabriel Pérez <gabriel.perez@porcelanosagrupo.com>
2023-02-03 17:50:20 +0100

    fix

--- app/helpers/marvel.php
+++ app/helpers/marvel.php
@@ -12,6 +12,7 @@ namespace Helpers\Marvel;
  * @return array
  */
 function mMd5(string $ts, string $publicKey): array {
+    $ts         = (string) round(microtime(true)*1000);
     $privateKey = env('MARVEL_PRIVATE_KEY');
     $has        = md5($ts . $privateKey . $publicKey);
 
